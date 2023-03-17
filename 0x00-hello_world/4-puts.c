a52f290a8d2a133039c6fa5eebbb2ace95a89b75
saj101 <mashakasajidu@gmail.com>
2023-03-16 17:26:52 -0700

    hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main-starting point
+*returns 0 of successful
+*/
+int main(void)/*starting point of the program
+*/
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);/* returns o if successfull runs
+*/
+}
