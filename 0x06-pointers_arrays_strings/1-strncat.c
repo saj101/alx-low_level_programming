1ad970b8f83fd021e80ccfd302091e6881be375a
saj101 <mashakasajidu@gmail.com>
2023-03-30 06:34:08 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,16 +1,17 @@
 #include "main.h"
-
 /**
- *_strncat - concatenates two strings
- *@dest:input
- *@src: input
- *@n: input
- *Return: dest
+ * _strncat - concatenate two strings
+ * using at most n bytes from src
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Return: dest
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+	int i;
+	int j;
 
 	i = 0;
 	while (dest[i] != '\0')
@@ -20,11 +21,10 @@ char *_strncat(char *dest, char *src, int n)
 	j = 0;
 	while (j < n && src[j] != '\0')
 	{
-		dest[i] = src[j];
-		i++;
-		j++;
+	dest[i] = src[j];
+	i++;
+	j++;
 	}
-	dest[i] = '\0'
+	dest[i] = '\0';
 	return (dest);
-
 }
