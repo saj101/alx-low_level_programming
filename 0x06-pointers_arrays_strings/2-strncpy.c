4c4c81f75dc33f0f7e62d93f79eb99e8868e4dc4
saj101 <mashakasajidu@gmail.com>
2023-03-30 06:39:10 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,9 +2,9 @@
 
 /**
  *_strncpy -copies a string
- *@dest: input value
- *@src: input value
- *@n: input value
+ *@dest: input
+ *@src: input
+ *@n: input 
  *Return: dest
  */
 
@@ -13,7 +13,7 @@ char *_strncpy(char *dest, char *src, int n)
 	int j;
 
 	j = 0;
-	while (j < n && src != '\0')
+	while (j < n && src[j] != '\0')
 	{
 		dest[j] = src[j];
 		j++;
