b7bdef49719c91d859ccb1533d2472b4d35cb56f
saj101 <mashakasajidu@gmail.com>
2023-03-30 06:59:46 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *cap_string - capitalizes all words
- *@str - string
+ *cap_string - capitalizes all words of a string
+ *@str: string to be capitalized
  *
- *Return : string
+ *Return: a pointer to the changed string
  */
 
 char *cap_string(char *str)
