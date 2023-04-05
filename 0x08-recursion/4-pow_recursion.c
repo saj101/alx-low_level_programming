6195c831360c9e4d368392117624035758fb4cbc
saj101 <mashakasajidu@gmail.com>
2023-04-05 15:58:26 -0700

    0x08. C - Recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,9 +8,9 @@
  */
 int _pow_recursion(int x, int y)
 {
-	for (y < 0)
+	if (y < 0)
+		return (-1);
+	if (y == 0)
 		return (1);
-	for (y == 0)
-		return (0);
 	return (x * _pow_recursion(x, y - 1));
 }
