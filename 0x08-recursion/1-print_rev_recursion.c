cc76a38697bb5e83f92f1afbbfddbead88242e5c
saj101 <mashakasajidu@gmail.com>
2023-04-05 15:44:33 -0700

    0x08. C - Recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,7 +4,7 @@
  *_print_rev_recursion - a function that prints a string in reverse
  *@s: input
  *
- *Return: Always 0.
+ *Return: reverse string.
  */
 void _print_rev_recursion(char *s)
 {
@@ -13,5 +13,5 @@ void _print_rev_recursion(char *s)
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
-	return (0);
+	
 }
