72e847c3b3a1a7b8ca46e4083dda2907cc7f8842
saj101 <mashakasajidu@gmail.com>
2023-04-30 08:25:50 -0700

    added a 1-listint_len.c file

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ *listint_len - returns the number of elements in a linked list
+ *
+ *@h: pointer to the head of the list
+ *Return: length of the list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t n = 0;
+	const listint_t *current = h;
+
+	while (current != NULL)
+	{
+		n++;
+		current = current->next;
+	}
+	return (n)
+}
