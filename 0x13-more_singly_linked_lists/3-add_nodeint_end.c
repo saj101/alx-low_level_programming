0ecfa52e3eeb36249cd6a2f5f69611107d8763c6
saj101 <mashakasajidu@gmail.com>
2023-04-30 10:42:51 -0700

    added a 3-add_nodeint_end.c file

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -26,7 +26,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		current = *head;
 		while (current->next != NULL)
 		{
-			current =current->next;
+			current = current->next;
 		}
 		current->next = new_node;
 	}
