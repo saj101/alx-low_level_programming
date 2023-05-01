61ef0bab2b035410c69e126684ebee1bb299532a
saj101 <mashakasajidu@gmail.com>
2023-04-30 23:37:11 -0700

    added a 4-free_listint.c file

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -4,23 +4,17 @@
  *free_listint - frees a list
  *@head: pointer to the list
  *
- *Return: returns a pointer to the node
+ *Return: none
  */
 void free_listint(listint_t *head)
 {
-	listint_t *new_node;
-
-	new_node = malloc(sizeof(listint_t));
-
-	while (*head == NULL)
-		return (NULL);
-	while (new_node == NULL)
-		return (NULL);
-	*head = new_node;
-
-	new_node->n = n;
-	new_node->next = NULL;
-
-	free(new_node);
-	return (new_node);
+	listint_t *next_node, *current;
+	
+	current = head;
+	while (current != NULL)
+	{
+		next_node = current->next;
+		free(current);
+		current = next_node;
+	}
 }
