ccd997924013975998880947d36f272f43e2e874
saj101 <mashakasajidu@gmail.com>
2023-05-01 03:04:19 -0700

    edited a 4-free_listint.c file

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,16 +2,16 @@
 
 /**
  * free_listint - frees a linked list
- * @head: listint_t list to be freed
+ * @head: pointer to pointer of head of list
  */
 void free_listint(listint_t *head)
 {
-	listint_t *temp;
+        listint_t *temp;
 
-	while (head)
-	{
-		temp = head->next;
-		free(head);
-		head = temp;
-	}
+        while (head != NULL)
+        {
+                temp = head;
+                head = head->next;
+                free(temp);
+        }
 }
