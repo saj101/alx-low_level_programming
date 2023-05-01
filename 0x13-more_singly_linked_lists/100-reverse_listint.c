5361424b5f2ba352cbde6a2c0b0dbc9ee21c7844
saj101 <mashakasajidu@gmail.com>
2023-05-01 04:41:31 -0700

    added the 100-reverse_listint.c, 101-print_listint_safe.c, 102-free_listint_safe.c, 103-find_loop.c files

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,24 +1,25 @@
 #include "lists.h"
 
 /**
- * reverse_listint - Reverses a listint_t linked list.
- * @head: A pointer to a pointer to the head node of the list.
+ * reverse_listint - reverses a linked list
+ * @head: pointer to the first node in the list
  *
- * Return: A pointer to the first node of the reversed list.
+ * Return: pointer to the first node in the new list
  */
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev = NULL;
-	listint_t *curr = *head;
 	listint_t *next = NULL;
 
-	while (curr != NULL)
+	while (*head)
 	{
-		next = curr->next;
-		curr->next = prev;
-		prev = curr;
-		curr = next;
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
 	}
+
 	*head = prev;
+
 	return (*head);
 }
