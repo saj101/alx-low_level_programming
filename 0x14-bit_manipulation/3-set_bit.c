f268e5a9dc26b7de04055ac0514710d6d4549cca
saj101 <mashakasajidu@gmail.com>
2023-05-04 11:48:57 -0700

    added a 3-set_bit.c file

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,6 +2,7 @@
 
 /**
  * set_bit - Sets the value of a bit to 1 at a given index.
+ *
  * @n: The number to modify.
  * @index: The index, starting from 0 of the bit to set.
  *
@@ -12,6 +13,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
-	*n = (*n | (1 << index));
+	*n |= 1 << index;
+
 	return (1);
 }
